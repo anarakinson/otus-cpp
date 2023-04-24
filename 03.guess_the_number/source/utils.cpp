9601b88f4578d377b5b8f46e7222f1d1525364a9
anarakinson <anarakinson@gmail.com>
2023-04-24 15:20:04 +0300

    fix parsing error (string to int convert)

--- 03.guess_the_number/source/utils.cpp
+++ 03.guess_the_number/source/utils.cpp
@@ -111,6 +111,6 @@ void show_results(const std::string &path) {
 
 // check if string is convertable to int
 bool is_number(const std::string &str) {
-    bool success = strspn( str.c_str(), "-.0123456789" ) == str.size();
+    bool success = strspn(str.c_str(), "-.0123456789") == str.size();
     return success;
 }
