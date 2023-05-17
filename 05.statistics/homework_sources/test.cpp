8be5c38db682cdd3ec87b8ba8faaddbbd9f29077
anarakinson <anarakinson@gmail.com>
2023-05-17 15:09:49 +0300

    template for time measure

--- 05.statistics/homework_sources/test.cpp
+++ 05.statistics/homework_sources/test.cpp
@@ -19,7 +19,6 @@ void make_random_shuffle(std::vector<T> &values) {
 
     // chrono system clock
     auto seconds_count = std::chrono::system_clock::now().time_since_epoch().count();
-    std::cout << std::chrono::system_clock::now().time_since_epoch().count() << std::endl;
     unsigned int seed = static_cast<unsigned int>(seconds_count);
     // random engine
     std::default_random_engine engine{seed};
@@ -29,6 +28,45 @@ void make_random_shuffle(std::vector<T> &values) {
 }
 
 
+// std::sort algorithm measuring
+void std_sort(std::vector<int> values) {
+    std::vector<int> test_data = values;
+    std::sort(test_data.begin(), test_data.end());
+}
+
+// C-style quick-sort algorithm measuring
+void clike_sort(std::vector<int> values) {
+    std::vector<int> test_data = values;
+    std::qsort(
+        test_data.data(), 
+        test_data.size(),
+        sizeof(int),
+        [](const void *x, const void *y) {
+            const int arg1 = *static_cast<const int *>(x);
+            const int arg2 = *static_cast<const int *>(y);
+            if (arg1 > arg2) { return 1; }
+            else if (arg1 < arg2) { return -1; }
+            return 0;
+        }
+    );
+}
+
+
+// template for time measure
+template <void(*sort_function)(std::vector<int>)>
+void time_measure(size_t iteration_count, std::vector<int> values) {
+
+    auto start_time = std::chrono::system_clock::now(); // get start time
+    for (size_t i = 0; i < iteration_count; ++i) {
+        sort_function(values);
+    }
+    auto end_time = std::chrono::system_clock::now(); // get end time
+    // Calculate a duration
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    std::cout << "std::sort duration = " << milliseconds.count() << "ms" << std::endl;
+    std::cout << "std::sort avg = " << milliseconds.count() / iteration_count << "ms" << std::endl;
+
+}
 
 
 void run_test_suite(size_t iteration_count) {
@@ -37,46 +75,17 @@ void run_test_suite(size_t iteration_count) {
     std::vector<int> values;
 
     fill_vector(values, elements_count);
+    // random shuffle values
     make_random_shuffle(values);
 
 
     std::cout << "iteration count: " << iteration_count << std::endl;
 
-    {
-        auto start_time = std::chrono::system_clock::now();
-        for (size_t i = 0; i < iteration_count; ++i) {
-            std::vector<int> test_data = values;
-            std::sort(test_data.begin(), test_data.end());
-        }
-        auto end_time = std::chrono::system_clock::now();
-        auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-        std::cout << "std::sort duration = " << milliseconds.count() << "ms" << std::endl;
-        std::cout << "std::sort avg = " << milliseconds.count() / iteration_count << "ms" << std::endl;
-    }
-
-
-    {
-        auto start_time = std::chrono::system_clock::now();
-        for (size_t i = 0; i < iteration_count; ++i) {
-            std::vector<int> test_data = values;
-            std::qsort(
-                test_data.data(), 
-                test_data.size(),
-                sizeof(int),
-                [](const void *x, const void *y) {
-                    const int arg1 = *static_cast<const int *>(x);
-                    const int arg2 = *static_cast<const int *>(y);
-                    if (arg1 > arg2) { return 1; }
-                    else if (arg1 < arg2) { return -1; }
-                    return 0;
-                }
-            );
-        }
-        auto end_time = std::chrono::system_clock::now();
-        auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-        std::cout << "std::sort duration = " << milliseconds.count() << "ms" << std::endl;
-        std::cout << "std::sort avg = " << milliseconds.count() / iteration_count << "ms" << std::endl;
-    }
+	// std::sort algorithm measuring
+    time_measure<std_sort>(iteration_count, values);
+    
+	// C-style quick-sort algorithm measuring
+    time_measure<clike_sort>(iteration_count, values);
 
 }
 
@@ -89,8 +98,10 @@ int main() {
     make_random_shuffle(vec);
 
     for (int val : vec) {
-        std::cout << val << std::endl;
+        std::cout << val << " ";
     }
+
+    std::cout << std::endl << std::endl;
 
     run_test_suite(10);
 }
